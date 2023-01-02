656c404a5c6b4c7729332b8ce640420e89b19527
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2023-01-02 22:27:02 +0600

    number type

--- dist/app.js
+++ dist/app.js
@@ -81,3 +81,12 @@ function logToppings(_a) {
     console.log(first, second, third);
 }
 logToppings(toppings);
+// 
+// primitive types in typescript
+var pizzaCost = 10;
+var pizzaToppings = 2;
+function calculatePrice(cost, toppings) {
+    return cost + 1.5 * toppings;
+}
+var cost = calculatePrice(pizzaCost, pizzaToppings);
+console.log("Pizza costs: ".concat(cost));
--- src/app.ts
+++ src/app.ts
@@ -115,3 +115,18 @@ function logToppings([first, second, third]: any) {
 }
 
 logToppings(toppings);
+
+
+// 
+// primitive types in typescript
+
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
+
+function calculatePrice(cost: number, toppings: number): number {
+  return cost + 1.5 * toppings;
+}
+
+const cost: number = calculatePrice(pizzaCost, pizzaToppings)
+
+console.log(`Pizza costs: ${cost}`);
